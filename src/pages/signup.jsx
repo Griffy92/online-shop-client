4221bc584859fe3f0c75d71af515a0891cbed674
Dominic <dominicwong09@gmail.com>
2023-06-04 20:12:22 +1200

    navbar/layout templates created

---
+++ src/pages/signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout'
+
+const Signup = () => {
+
+    return (
+        <Layout>
+            <h1>Sign up here foo</h1>
+        </Layout>
+    )
+}
+
+export default Signup;
