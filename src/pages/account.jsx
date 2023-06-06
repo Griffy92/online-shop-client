b7326e09e44b16c0ecba84cea752922af07fd88e
Dominic <dominicwong09@gmail.com>
2023-06-07 10:18:54 +1200

    Account page created w/ dynamic user info. Order history pending

--- src/pages/account.jsx
+++ src/pages/account.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import { useContext } from 'react';
 import { UserContext } from '../providers/UserProvider'
 
@@ -21,13 +20,6 @@ const Account = () => {
                     <li>contact number: {user.contact_number}</li>
                 </ul>
             </div>
-=======
-
-const Account = () => {
-    return (
-        <>
-            <h1>Account Page</h1>
->>>>>>> a125cbe (Wrap page/root element + signin functioning)
         </>
     )
 }
