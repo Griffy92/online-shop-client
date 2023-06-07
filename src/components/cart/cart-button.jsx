876f0b85bb3067737043c604dd4750037af2daeb
EdgarAllenExile <riley.mercer4@gmail.com>
2023-06-07 09:14:25 +0800

    conflict resolution

--- src/components/cart/cart-button.jsx
+++ src/components/cart/cart-button.jsx
@@ -44,11 +44,7 @@ const CartButton = () => {
                 </div>
                 <hr />
                 <div className='flex justify-center p-2'>
-<<<<<<< HEAD
                     <button className="btn btn-primary btn-wide btn-md ">Checkout</button>
-=======
-                    <button className="btn btn-primary btn-wide btn-md">Checkout</button>
->>>>>>> 8d5220d (re-making previously conflicted changes - cart panel styling)
                 </div>
             </Popover.Panel>
       </Popover>
