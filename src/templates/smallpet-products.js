6a8a4fda3b4a27554e75435f50f9d6356a18e91c
Roxanne Liu <roxanneliu819@gmail.com>
2023-06-09 01:42:34 +1000

    make small pet oage card & bug found at seed data

--- src/templates/smallpet-products.js
+++ src/templates/smallpet-products.js
@@ -5,20 +5,24 @@ const SmallpetProductsTemplate = ({ pageContext }) => {
   const { smallpetProducts } = pageContext;
 
   return (
-    <div>
-      <h1>Small Pet Products</h1>
-      <ul>
-        {smallpetProducts.map(product => (
-          <li key={product.id}>
-            <Link to={`/product/${product.id}`}>
-              <img src={`http://localhost:3000${product.image}`} alt={product.product_name} />
-              {product.product_name} <br></br>
-              ${ product.retail_price }
-
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="container mx-auto">
+        <h1>Small Pet Products</h1>
+        <div className="bg-white place-content-center grid grid-cols-4 gap-4">
+            {smallpetProducts.map(product => (
+                <div key={product.id}>
+                    <Link to={`/product/${product.id}`}>
+                        <figure className="px-10 pt-10 w-100 h-100">
+                            <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover" />
+                        </figure>
+                        <h2 className="card-title text-black">{product.product_name}</h2>
+                        <p className="badge badge-secondary">${ product.retail_price }</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Add to Cart</button>
+                        </div>
+                    </Link>
+                </div>
+            ))}
+        </div>
     </div>
   );
 };
