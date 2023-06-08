bf3d8625c5bc2ca19e61ef69558cf3fc1c14cd8e
Roxanne Liu <roxanneliu819@gmail.com>
2023-06-09 01:13:21 +1000

    make bird-products page product card display

--- src/templates/bird-products.js
+++ src/templates/bird-products.js
@@ -5,20 +5,24 @@ const BirdProductsTemplate = ({ pageContext }) => {
   const { birdProducts } = pageContext;
 
   return (
-    <div>
-      <h1>Bird Products</h1>
-      <ul>
-        {birdProducts.map(product => (
-          <li key={product.id}>
-            <Link to={`/product/${product.id}`}>
-              <img src={`http://localhost:3000${product.image}`} alt={product.product_name} />
-              {product.product_name} <br></br>
-              ${ product.retail_price }
-
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="container mx-auto">
+        <h1>Bird Products</h1>
+        <div className="bg-white place-content-center grid grid-cols-4 gap-4">
+            {birdProducts.map(product => (
+                <div key={product.id}>
+                    <Link to={`/product/${product.id}`}>
+                        <figure className="px-10 pt-10 w-100 h-100">
+                            <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover" />
+                        </figure>
+                        <h2 className="card-title text-black">{product.product_name}</h2>
+                        <p className="badge badge-secondary">${ product.retail_price }</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Add to Cart</button>
+                        </div>
+                    </Link>
+                </div>
+            ))}
+        </div>
     </div>
   );
 };
