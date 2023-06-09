d0a6cb1861df01dbb73322a0194c49b62c2e1937
Roxanne Liu <roxanneliu819@gmail.com>
2023-06-09 19:07:23 +1000

    made reusable filter and applied it to dog-products]

--- src/templates/dog-products.js
+++ src/templates/dog-products.js
@@ -1,31 +1,39 @@
-import React from "react";
+import React , { useState } from "react";
 import { Link } from "gatsby";
+import ProductFilter from "../components/product-filter";
 
 const DogProductsTemplate = ({ pageContext }) => {
-  const { dogProducts } = pageContext;
+    const { dogProducts } = pageContext;
 
-  return (
-    <div className="container mx-auto">
-        <h1>Dog Products</h1>
-        <div className="bg-white place-content-center grid grid-cols-4 gap-4">
-            {dogProducts.map(product => (
-                <div key={product.id}>
-                    <Link to={`/product/${product.id}`}>
-                        <figure className="px-10 pt-10 w-100 h-100">
-                            <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover" />
-                        </figure>
-                        <h2 className="card-title text-black">{product.product_name}</h2>
-                        <p className="badge badge-secondary">${ product.retail_price }</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Add to Cart</button>
-                        </div>
-                    </Link>
-                </div>
-            ))}
+    const [filteredProducts, setFilteredProducts] = useState(dogProducts);
+
+    const applyFilters = (updatedProducts) => {
+        setFilteredProducts(updatedProducts);
+    };
+
+    return (
+        <div className="container mx-auto">
+            <h1>Dog Products</h1>
+            <ProductFilter products={dogProducts} onApplyFilters={applyFilters} />
+            <div className="bg-white place-content-center grid grid-cols-4 gap-4">
+                {filteredProducts.map(product => (
+                    <div key={product.id}>
+                        <Link to={`/product/${product.id}`}>
+                            <figure className="px-10 pt-10 w-100 h-100">
+                                <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover" />
+                            </figure>
+                            <h2 className="card-title text-black">{product.product_name}</h2>
+                            <p className="badge badge-secondary">${ product.retail_price }</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary">Add to Cart</button>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-  );
-};
+    );
+    };
 
 export default DogProductsTemplate;
 
