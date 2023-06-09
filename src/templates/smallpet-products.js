8015faf9915e00976cea8568899487cffea6533c
Roxanne Liu <roxanneliu819@gmail.com>
2023-06-09 19:36:19 +1000

    made reusable filter applied to smallpet-products

--- src/templates/smallpet-products.js
+++ src/templates/smallpet-products.js
@@ -1,30 +1,38 @@
-import React from "react";
+import React , { useState } from "react";
 import { Link } from "gatsby";
+import ProductFilter from "../components/product-filter";
 
 const SmallpetProductsTemplate = ({ pageContext }) => {
-  const { smallpetProducts } = pageContext;
+    const { smallpetProducts } = pageContext;
 
-  return (
-    <div className="container mx-auto">
-        <h1>Small Pet Products</h1>
-        <div className="bg-white place-content-center grid grid-cols-4 gap-4">
-            {smallpetProducts.map(product => (
-                <div key={product.id}>
-                    <Link to={`/product/${product.id}`}>
-                        <figure className="px-10 pt-10 w-100 h-100">
-                            <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover" />
-                        </figure>
-                        <h2 className="card-title text-black">{product.product_name}</h2>
-                        <p className="badge badge-secondary">${ product.retail_price }</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Add to Cart</button>
-                        </div>
-                    </Link>
-                </div>
-            ))}
+    const [filteredProducts, setFilteredProducts] = useState(smallpetProducts);
+
+    const applyFilters = (updatedProducts) => {
+        setFilteredProducts(updatedProducts);
+    };
+
+    return (
+        <div className="container mx-auto">
+            <h1>Small Pet Products</h1>
+            <ProductFilter products={smallpetProducts} onApplyFilters={applyFilters} />
+            <div className="bg-white place-content-center grid grid-cols-4 gap-4">
+                {filteredProducts.map(product => (
+                    <div key={product.id}>
+                        <Link to={`/product/${product.id}`}>
+                            <figure className="px-10 pt-10 w-100 h-100">
+                                <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover" />
+                            </figure>
+                            <h2 className="card-title text-black">{product.product_name}</h2>
+                            <p className="badge badge-secondary">${ product.retail_price }</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary">Add to Cart</button>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default SmallpetProductsTemplate;
