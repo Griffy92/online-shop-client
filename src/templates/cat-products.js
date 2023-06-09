4aa7f56e4bbb0b468e8c365b794284079605ec24
Roxanne Liu <roxanneliu819@gmail.com>
2023-06-09 19:45:54 +1000

    add No Items Found to filter results - cat

--- src/templates/cat-products.js
+++ src/templates/cat-products.js
@@ -16,20 +16,24 @@ const CatProductsTemplate = ({ pageContext }) => {
             <h1>Cat Products</h1>
             <ProductFilter products={catProducts} onApplyFilters={applyFilters} />
             <div className="bg-white place-content-center grid grid-cols-4 gap-4">
-                {filteredProducts.map(product => (
-                    <div key={product.id}>
-                        <Link to={`/product/${product.id}`}>
-                            <figure className="px-10 pt-10 w-100 h-100">
-                                <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover" />
-                            </figure>
-                            <h2 className="card-title text-black">{product.product_name}</h2>
-                            <p className="badge badge-secondary">${ product.retail_price }</p>
-                            <div className="card-actions">
-                                <button className="btn btn-primary">Add to Cart</button>
-                            </div>
-                        </Link>
-                    </div>
-                ))}
+                {filteredProducts.length > 0 ? (
+                    filteredProducts.map(product => (
+                        <div key={product.id}>
+                            <Link to={`/product/${product.id}`}>
+                                <figure className="px-10 pt-10 w-100 h-100">
+                                    <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover" />
+                                </figure>
+                                <h2 className="card-title text-black">{product.product_name}</h2>
+                                <p className="badge badge-secondary">${ product.retail_price }</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary">Add to Cart</button>
+                                </div>
+                            </Link>
+                        </div>
+                    ))
+                ) : (
+                    <h2>No Items Found</h2>
+                )}
             </div>
         </div>
     );
