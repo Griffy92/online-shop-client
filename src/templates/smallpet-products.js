e130a67b59438adf2389da33a90a4d0fe557f5de
Roxanne Liu <roxanneliu819@gmail.com>
2023-06-12 00:09:10 +1000

    fix the products item list width in the smallpet-products page

--- src/templates/smallpet-products.js
+++ src/templates/smallpet-products.js
@@ -15,25 +15,27 @@ const SmallpetProductsTemplate = ({ pageContext }) => {
         <div className="container mx-auto">
             <h1>Small Pet Products</h1>
             <ProductFilter products={smallpetProducts} onApplyFilters={applyFilters} />
-            <div className="bg-white place-content-center grid grid-cols-4 gap-4">
-                {filteredProducts.length > 0 ? (
-                    filteredProducts.map(product => (
-                        <div key={product.id}>
-                            <Link to={`/product/${product.id}`}>
-                                <figure className="px-10 pt-10 w-100 h-100">
-                                    <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover" />
-                                </figure>
-                                <h2 className="card-title text-black">{product.product_name}</h2>
-                                <p className="badge badge-secondary">${ product.retail_price }</p>
-                                <div className="card-actions">
-                                    <button className="btn btn-primary">Add to Cart</button>
-                                </div>
-                            </Link>
-                        </div>
-                    ))
-                ) : (
-                    <h2>No Items Found</h2>
-                )}
+            <div className="ml-64">
+                <div className="bg-white place-content-center grid grid-cols-4 gap-4">
+                    {filteredProducts.length > 0 ? (
+                        filteredProducts.map(product => (
+                            <div key={product.id} className="card card-compact bg-white shadow-xl p-4">
+                                <Link to={`/product/${product.id}`}>
+                                    <figure className="px-10 pt-10 w-100 h-100">
+                                        <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover" />
+                                    </figure>
+                                    <h2 className="card-title text-black">{product.product_name}</h2>
+                                    <p className="badge badge-secondary">${ product.retail_price }</p>
+                                    <div className="card-actions">
+                                        <button className="btn btn-primary">Add to Cart</button>
+                                    </div>
+                                </Link>
+                            </div>
+                        ))
+                    ) : (
+                        <h2>No Items Found</h2>
+                    )}
+                </div>
             </div>
         </div>
     );
