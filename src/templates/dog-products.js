e37b6a0d56aa2a2eef4176f6fc77d9bfefead94d
Roxanne Liu <roxanneliu819@gmail.com>
2023-06-12 23:47:40 +1000

    dog-products pg looks better now

--- src/templates/dog-products.js
+++ src/templates/dog-products.js
@@ -19,14 +19,16 @@ const DogProductsTemplate = ({ pageContext }) => {
                 <div className="bg-white place-content-center grid grid-cols-4 gap-4">
                     {filteredProducts.length > 0 ? (
                         filteredProducts.map(product => (
-                            <div key={product.id} className="card card-compact bg-white shadow-xl p-4">
+                            <div key={product.id} className="card card-compact bg-white shadow-2xl p-4">
                                 <Link to={`/product/${product.id}`}>
-                                    <figure className="px-10 pt-10 w-100 h-100">
-                                        <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover" />
+                                    <figure className="px-10 pt-10">
+                                        <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover product-img" />
                                     </figure>
                                 </Link>
-                                <h2 className="card-title text-black">{ product.product_name }</h2>
-                                <p className="badge badge-secondary">${ product.retail_price }</p>
+                                <div className="product-title">
+                                    <h2 className="card-title text-black">{ product.product_name }</h2>
+                                    <p className="badge badge-secondary">${ product.retail_price }</p>
+                                </div>
                                 <div className="w-full mx-auto">
                                     <ProductControls product={product}/>
                                 </div>
