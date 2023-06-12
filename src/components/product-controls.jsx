a639126e06f7963c0bc00ed65130ef4f6a0fde6d
Roxanne Liu <roxanneliu819@gmail.com>
2023-06-12 23:43:18 +1000

    bird-products page looks better

--- src/components/product-controls.jsx
+++ src/components/product-controls.jsx
@@ -129,9 +129,6 @@ const ProductControls = ({product}) => {
     return (
         <div className="w-full mx-auto">
             <button className="btn btn-success w-full" onClick={_handleAddCart}>Add to Cart</button>
-            {/* <button className="btn btn-error" onClick={_handleRemoveCart}>
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
-            </button> */}
         </div>
     )
 };
