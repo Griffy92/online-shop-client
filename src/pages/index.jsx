d3a640e3f9579bcf8a2bc67aeef6e0ff6543b39f
kaarenliangg <karenliang9546@gmail.com>
2023-06-12 19:09:14 +1000

    WIP header image

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
 import IndexCarousel from "../components/index-carousel/index-carousel"
-import headerImage from '../images/header.png';
+import headerImage from '../images/header-temp.jpg';
 
 const IndexPage = () => {
 
