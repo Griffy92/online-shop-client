6eef796d3bae81bb784254a827c4ee1c12f924d0
kaarenliangg <karenliang9546@gmail.com>
2023-06-14 23:30:27 +1000

    product cart and title update on search

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -70,12 +70,26 @@ const Search = () => {
                                             />
                                         </figure>
                                     </Link>
+
                                     <div className="product-title">
-                                        <h2 className="card-title text-black">{ product.product_name }</h2>
+                                        <h2 
+                                            className="card-title text-black font-poppinsSemi font-semi-bold" 
+                                            style={{
+                                                fontSize: `13pt`, 
+                                                lineHeight: `1.4`}}>
+                                                    { product.product_name }
+                                        </h2>
                                     </div>
 
                                     <div className="w-full mx-auto">
-                                        <p className="badge badge-secondary block mt-3 mb-3">${ product.retail_price }</p>
+                                        <p 
+                                            className="badge block mt-3 mb-3 font-poppinsMed font-medium bg-sky-500 text-s" 
+                                            style={{
+                                                backgroundColor: `#8986FF`, 
+                                                color: `white`, 
+                                                border: `1px solid transparent`, 
+                                            }}>
+                                            ${ product.retail_price }</p>
                                         <br></br>
                                             <ProductControls product={product}/>
                                     </div>
