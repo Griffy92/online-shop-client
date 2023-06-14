5bdc3dd904fdd714f713cfebc0801355a1098fab
Dominic <dominicwong09@gmail.com>
2023-06-14 22:29:46 +1200

    User accounts page updated

--- src/pages/account.jsx
+++ src/pages/account.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import { useContext } from 'react';
 import { UserContext } from '../providers/UserProvider'
+import UserOrders from '../components/ UserOrderHistory';
+import { useEffect } from 'react';
 
 
 const Account = () => {
     const { user } = useContext(UserContext);
-    console.log(user)
+
+    useEffect( () => {
+
+    })
 
     return (
-        <>
-            <div>
-                <h2>Order History Coming</h2>
-            </div>
-            <div>
-                <h2>Account Details</h2>
-                <ul>
-                    <li>Name: {user.fullname}</li>
-                    <li>email: {user.email} </li>
-                    <li>address: {user.address} </li>
-                    <li>contact number: {user.contact_number}</li>
-                </ul>
+        <>  
+            <div className="container mx-auto"> 
+                <div className="mt-10 mb-5 text-3xl font-extrabold font-poppins text-black">
+                    <h1>My Account</h1>
+                </div>
+                <div className="flex justify-around">
+                    <div>
+                        <h2>Account Details</h2>
+                        <ul>
+                            <li>Name: {user.fullname}</li>
+                            <li>Email: {user.email} </li>
+                            <li>Address: {user.address} </li>
+                            <li>Contact Number: {user.contact_number}</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <UserOrders />
+                    </div>
+                </div>
             </div>
         </>
     )
