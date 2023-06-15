9824e4dd9b12cd7b59ca3fabef162a14e2c9adbd
kaarenliangg <karenliang9546@gmail.com>
2023-06-15 22:42:06 +1000

    major update - all admin pages inc products and order form updated

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -48,34 +48,34 @@ const Signup = () => {
         <>
         <section 
             onSubmit={ _handleUserCreationSubmit } 
-            class="bg-gray-50 dark:bg-gray-900"
+            className="bg-gray-50 dark:bg-gray-900"
             style={{
                 backgroundImage: `url(${accountImage})`,
                 backgroundSize: `cover`,
                 marginTop: `-5%`
                 }}
         >
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Create an Account
                         </h1>
                         <form 
-                            class="space-y-4 md:space-y-6" 
+                            className="space-y-4 md:space-y-6" 
                             onSubmit={ _handleUserCreationSubmit} 
                         >
                             <div>
                                 <label 
                                     for="email" 
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name
                                 </label>
 
                                 <input 
                                     name="fullname" 
                                     value={ name } 
                                     onChange={ _handleNameChange } 
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" style={{ 
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" style={{ 
                                             WebkitTextFillColor: `black`, 
                                             WebkitBoxShadow: `0 0 0px 1000px white inset` 
                                     }}
@@ -87,7 +87,7 @@ const Signup = () => {
                             <div>
                                 <label 
                                     for="email" 
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                         Email
                                 </label>
 
@@ -95,7 +95,7 @@ const Signup = () => {
                                     name="email" 
                                     value={ email } 
                                     onChange={ _handleEmailChange } 
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                                     style={{ 
                                         WebkitTextFillColor: `black`, 
                                         WebkitBoxShadow: `0 0 0px 1000px white inset` 
@@ -108,7 +108,7 @@ const Signup = () => {
                             <div>
                                 <label 
                                     for="password" 
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                         Password
                                 </label>
 
@@ -117,28 +117,28 @@ const Signup = () => {
                                     value={ password } 
                                     onChange={ _handlePasswordChange } 
                                     placeholder="••••••••" 
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                                     required=""
                                 />
                             </div>
 
-                            <div class="flex items-start">
-                                <div class="flex items-center h-5">
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
                                     <input 
                                         id="terms" 
                                         aria-describedby="terms" 
                                         type="checkbox" 
-                                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" 
+                                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" 
                                         required=""
                                     />
                                 </div>
 
-                                <div class="ml-3 text-sm">
+                                <div className="ml-3 text-sm">
                                     <label 
                                         for="terms" 
-                                        class="font-light text-gray-500 dark:text-gray-300">I accept the 
+                                        className="font-light text-gray-500 dark:text-gray-300">I accept the 
                                             <a 
-                                                class="font-medium text-primary-600 hover:underline dark:text-primary-500" 
+                                                className="font-medium text-primary-600 hover:underline dark:text-primary-500" 
                                                 href="#">Terms and Conditions
                                             </a>
                                     </label>
@@ -150,11 +150,11 @@ const Signup = () => {
                                     Create an account
                             </button>
 
-                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Already have an account? 
                                     <a 
                                         href="/signin" 
-                                        class="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                                        className="font-medium text-primary-600 hover:underline dark:text-primary-500">
                                             Login here 
                                     </a>
                             </p>
