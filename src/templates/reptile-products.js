7f1ebcf453e8d41d10f0a0d32589a5e33e717399
Roxanne Liu <roxanneliu819@gmail.com>
2023-06-15 11:00:28 +1000

    fixed indentation of all pages

--- src/templates/reptile-products.js
+++ src/templates/reptile-products.js
@@ -21,47 +21,47 @@ const ReptileProductsTemplate = ({ pageContext }) => {
                 style={{
                     marginTop: `85px`,
                     color: `#383838`,
-                }}
-                > Reptile Products
+                }}> 
+                Reptile Products
                 </h1>
-                    <div className="place-content-center grid grid-cols-4 gap-4">
-                        {filteredProducts.length > 0 ? (
-                            filteredProducts.map(product => (
-                                <div key={product.id} className="card card-compact bg-white shadow-2xl p-4">
-                                    <Link to={`/product/${product.id}`} >
-                                        <figure className="px-10 pt-10">
-                                            <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover product-img" />
-                                        </figure>
-                                    </Link>
-                                    <div className="product-title">
-                                        <h2 
-                                            className="card-title text-black font-poppinsSemi font-semi-bold" 
-                                            style={{
-                                                fontSize: `13pt`, 
-                                                lineHeight: `1.4`
-                                            }}>
-                                            { product.product_name }
-                                        </h2>                              
-                                    </div>
-                                    <div className="w-full mx-auto">
-                                        <p 
-                                            className="badge block mt-3 mb-3 font-poppinsMed font-medium bg-sky-500 text-s" 
-                                            style={{
-                                                backgroundColor: `#8986FF`, 
-                                                color: `white`, 
-                                                border: `1px solid transparent`, 
-                                            }}>
-                                            ${ product.retail_price }
-                                        </p>
-                                        <br></br>
-                                        <ProductControls product={product}/>
-                                    </div>
+                <div className="place-content-center grid grid-cols-4 gap-4">
+                    {filteredProducts.length > 0 ? (
+                        filteredProducts.map(product => (
+                            <div key={product.id} className="card card-compact bg-white shadow-2xl p-4">
+                                <Link to={`/product/${product.id}`} >
+                                    <figure className="px-10 pt-10">
+                                        <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover product-img" />
+                                    </figure>
+                                </Link>
+                                <div className="product-title">
+                                    <h2 
+                                        className="card-title text-black font-poppinsSemi font-semi-bold" 
+                                        style={{
+                                            fontSize: `13pt`, 
+                                            lineHeight: `1.4`,
+                                        }}>
+                                        { product.product_name }
+                                    </h2>                              
                                 </div>
-                            ))
-                        ) : (
-                            <h2>No Items Found</h2>
-                        )}
-                    </div>
+                                <div className="w-full mx-auto">
+                                    <p 
+                                        className="badge block mt-3 mb-3 font-poppinsMed font-medium bg-sky-500 text-s" 
+                                        style={{
+                                            backgroundColor: `#8986FF`, 
+                                            color: `white`, 
+                                            border: `1px solid transparent`, 
+                                        }}>
+                                        ${ product.retail_price }
+                                    </p>
+                                    <br></br>
+                                    <ProductControls product={product}/>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <h2>No Items Found</h2>
+                    )}
+                </div>
             </div>
         </div>
     );
--- src/templates/smallpet-products.js
+++ src/templates/smallpet-products.js
@@ -20,46 +20,48 @@ const SmallpetProductsTemplate = ({ pageContext }) => {
                     class="mb-5 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-5xl dark:text-white" 
                     style={{
                         marginTop: `85px`,
-                        color: `#383838`,}}
-                    > Small Pet Products
+                        color: `#383838`,
+                    }}>
+                    Small Pet Products
                 </h1>
-                    <div className="bg-white place-content-center grid grid-cols-4 gap-4">
-                        {filteredProducts.length > 0 ? (
-                            filteredProducts.map(product => (
-                                <div key={product.id} className="card card-compact bg-white shadow-2xl p-4">
-                                    <Link to={`/product/${product.id}`}>
-                                        <figure className="px-10 pt-10">
-                                            <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover product-img" />
-                                        </figure>
-                                    </Link>
-                                    <div className="product-title">
-                                        <h2 
-                                            className="card-title text-black font-poppinsSemi font-semi-bold" 
-                                            style={{
-                                                fontSize: `13pt`, 
-                                                lineHeight: `1.4`}}>
-                                                {product.product_name}
-                                        </h2>
-                                    </div>
-                                    <div className="w-full mx-auto">
-                                        <p 
-                                            className="badge block mt-3 mb-3 font-poppinsMed font-medium bg-sky-500 text-s" 
-                                            style={{
-                                                backgroundColor: `#8986FF`, 
-                                                color: `white`, 
-                                                border: `1px solid transparent`, 
-                                            }}>
-                                                ${ product.retail_price }
-                                        </p>
-                                        <br></br>
-                                        <ProductControls product={product}/>
-                                    </div>
+                <div className="bg-white place-content-center grid grid-cols-4 gap-4">
+                    {filteredProducts.length > 0 ? (
+                        filteredProducts.map(product => (
+                            <div key={product.id} className="card card-compact bg-white shadow-2xl p-4">
+                                <Link to={`/product/${product.id}`}>
+                                    <figure className="px-10 pt-10">
+                                        <img src={`http://localhost:3000${product.image}`} alt={product.product_name} className="w-full h-60 object-cover product-img" />
+                                    </figure>
+                                </Link>
+                                <div className="product-title">
+                                    <h2 
+                                        className="card-title text-black font-poppinsSemi font-semi-bold" 
+                                        style={{
+                                            fontSize: `13pt`, 
+                                            lineHeight: `1.4`,
+                                        }}>
+                                        {product.product_name}
+                                    </h2>
                                 </div>
-                            ))
-                        ) : (
-                            <h2>No Items Found</h2>
-                        )}
-                    </div>
+                                <div className="w-full mx-auto">
+                                    <p 
+                                        className="badge block mt-3 mb-3 font-poppinsMed font-medium bg-sky-500 text-s" 
+                                        style={{
+                                            backgroundColor: `#8986FF`, 
+                                            color: `white`, 
+                                            border: `1px solid transparent`, 
+                                        }}>
+                                        ${ product.retail_price }
+                                    </p>
+                                    <br></br>
+                                    <ProductControls product={product}/>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <h2>No Items Found</h2>
+                    )}
+                </div>
             </div>
         </div>
     );
