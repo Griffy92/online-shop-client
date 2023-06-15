38c0e86c868477c4b89f79347d797b38ff24e8f8
Roxanne Liu <roxanneliu819@gmail.com>
2023-06-15 23:03:10 +1000

    filter updated

--- src/components/product-filter.jsx
+++ src/components/product-filter.jsx
@@ -23,7 +23,7 @@ const ProductFilter = ({ products, onApplyFilters }) => {
             updatedProducts.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
         };
 
-        applyFilters(updatedProducts);
+        onApplyFilters(updatedProducts);
     }
 
     const _handleApplyFilters = () => {
@@ -40,13 +40,13 @@ const ProductFilter = ({ products, onApplyFilters }) => {
                     xmlns="http://www.w3.org/2000/svg" 
                     fill="none" 
                     viewBox="0 0 24 24" 
-                    strokeWidth="1.5" 
+                    stroke-width="1.5" 
                     stroke="currentColor" 
                     className="w-6 h-6"
                 >
                     <path 
-                        strokeLinecap="round" 
-                        strokeLinejoin="round" 
+                        stroke-linecap="round" 
+                        stroke-linejoin="round" 
                         d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" 
                     />
                 </svg>
