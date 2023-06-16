1a80db7efc0d6fee8546f0795dec2a82d6fa22f4
EdgarAllenExile <riley.mercer4@gmail.com>
2023-06-16 09:59:22 +0800

    cart ordering

--- src/components/cart/cart-button-item-generator.jsx
+++ src/components/cart/cart-button-item-generator.jsx
@@ -4,6 +4,8 @@ import CartButtonItem from "./cart-button-item";
 // this components takes the full array of cartItems, passed to it and generates a table row for each item
 
 const CardButtonItemGenerator = ( {cartItems} ) => {
+
+    cartItems.sort((a, b) => a.product.value - b.product.value);
     
     return (
         <tbody>
